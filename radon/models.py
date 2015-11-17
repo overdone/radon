ad688556cfe29760312dbc3204e092a141828a2b
overdone <for.31337@gmail.com>
2015-11-17 12:06:57 +0300

    Start project. Init commit

---
+++ radon/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class RoamingReportRef(models.Model):
+    lcal_id = models.IntegerField()
+    group_id = models.IntegerField()
+    srls_id = models.IntegerField()
+    branch_id = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    insert_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:  
+        ordering = ["price"]
+        unique_together = ('lcal_id', 'group_id', 'srls_id', 'branch_id', 'price')
+
+
+class MproReportRef(models.Model):
+    service_id = models.IntegerField()
+    option_id = models.IntegerField()
+    kp_id = models.IntegerField()
+    owner_id = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    insert_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:  
+        ordering = ["service_id"]
+        unique_together = ('service_id', 'option_id', 'kp_id', 'price')
---
+++ radon/views.py
@@ -0,0 +1,4 @@
+from rest_framework import permissions, viewsets
+
+from radon.models import RoamingReportRef, MproReportRef
+from radon.serializers import RoamingReportRefSerializer
