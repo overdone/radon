c28d651f2ec361ad5565e40955dca182c17051c8
overdone <for.31337@gmail.com>
2015-12-30 01:26:36 +0300

    Add confirm deleting dialog

--- app/js/reports.js
+++ app/js/reports.js
@@ -8,11 +8,11 @@ app.controller('ReportListController', function(){
   this.reports = [{
     name: 'Отчёт WAP ДЗ', 
     type: 'fraud'
-    }, 
-    {
+}, 
+{
     name: 'Мониторинг Роуминга',
     type: 'RA'
-    }];
+}];
 });
 
 app.controller('RoamingReportController', ['$http', '$scope', 'ngDialog', function($http, $scope, ngDialog){
@@ -24,14 +24,14 @@ app.controller('RoamingReportController', ['$http', '$scope', 'ngDialog', functi
     /*
      * Get API
      */
-    var store = this;  
+     var store = this;  
     store.tablerows = [];  // store of DB table rows
     store.displayed = [];  // rows in current page
 
     $http.get('/api/v1/roaming.json')
-        .success(function(data) {
-            store.tablerows = data;     
-        });    
+    .success(function(data) {
+        store.tablerows = data;     
+    });    
 
     store.displayed = [].concat(store.tablerows);
     /* Get */
@@ -40,25 +40,30 @@ app.controller('RoamingReportController', ['$http', '$scope', 'ngDialog', functi
     /*
      * Delete API
      */
-    $scope.deleteRow = function(rowId, ind) {
+     $scope.deleteRow = function(rowId, ind) {
+        var res = ngDialog.openConfirm({
+            template: 'app/templates/confirm_delete.html'
+        }).then(function (value) {
+            $http.delete('/api/v1/roaming/' + rowId)
+              .success(function(data, status) {
+                 store.displayed.splice(ind, 1);
+             });
+        }, function (reason) {
 
-        $http.delete('/api/v1/roaming/' + rowId)
-             .success(function(data, status) {
-                store.displayed.splice(ind, 1);
-            });
+        });        
     };
-    /* Delete */
+/* Delete */
 
-    $scope.editRow = function(header, row) {
-        $scope.val = row || {};
-        $scope.header = header;
+$scope.editRow = function(header, row) {
+    $scope.val = row || {};
+    $scope.header = header;
 
-        ngDialog.open({ 
-            template: 'app/templates/roam_rep_modal.html',
-            controller: 'editWindowController',
-            scope: $scope
-        });
-    };
+    ngDialog.open({ 
+        template: 'app/templates/roam_rep_modal.html',
+        controller: 'editWindowController',
+        scope: $scope
+    });
+};
 
 
 
