85b89b32365942e47208337a7e01ac4689cb4d47
overdone <for.31337@gmail.com>
2016-01-19 21:05:57 +0300

    Menu down\up action

--- app/js/menu.js
+++ app/js/menu.js
@@ -6,53 +6,45 @@
         $scope.arrowClass = "arrow-down-icon"; // Default menu item arrow state
 
         $scope.sections = [
-            {name: 'Отчёты RA'},
-            {name: 'Отчёты FRAUD'}];
-
-        $scope.raReports = [
-        {
-            name: 'Мониторинг Роуминга',
-            type: 'RA',
-            url: '#roam_rep'
-        },
-        {
-            name: 'Payments',
-            type: 'RA',
-            url: '#payments'    
-        },
-        {
-            name: 'Adjustment',
-            type: 'RA',
-            url: '#adjust'    
-        }];
-
-        $scope.fraudReports = [{
-            name: 'Отчёт WAP ДЗ', 
-            type: 'fraud',
-            url: '#mpro'
-        },
-        {
-            name: 'ДЗ краткий', 
-            type: 'fraud',
-            url: '#dzshort'
-        },
-        {
-            name: 'Мошенники', 
-            type: 'fraud',
-            url: '#frauders'
-        }];
+            {
+                name: 'Отчёты RA',
+                active: false,
+                items: [
+                {
+                    name: 'Мониторинг Роуминга',
+                    type: 'RA',
+                    url: '#roam_rep'
+                },{
+                    name: 'Payments',
+                    type: 'RA',
+                    url: '#payments'    
+                },{
+                    name: 'Adjustment',
+                    type: 'RA',
+                    url: '#adjust'    
+                }]
+            },{
+                name: 'Отчёты FRAUD',
+                active: false,
+                items: [{
+                    name: 'Отчёт WAP ДЗ', 
+                    type: 'fraud',
+                    url: '#mpro'
+                },{
+                    name: 'ДЗ краткий', 
+                    type: 'fraud',
+                    url: '#dzshort'
+                },{
+                    name: 'Мошенники', 
+                    type: 'fraud',
+                    url: '#frauders'
+                }]
+            }
+        ];
 
         // Click menu item action
-        $scope.toggleMenu = function(section) {
-            $scope.selected = section;
-        };
-
-        $scope.isSelected = function(section) {
-            if ($scope.selected === section) {
-                return "arrow-up-icon";
-            } else {
-                return "arrow-down-icon";
-            }
+        $scope.toggleMenu = function(s) {
+            s.active = !s.active;
         };
 
     }]);
